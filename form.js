7bf96184b07bbef26a1b37df82d0dcbf3d52eaf4
Usheninte <jrandangana@gmail.com>
2018-07-14 10:22:18 +0100

    confidence++ in writing react code

---
+++ form.js
@@ -0,0 +1,36 @@
+//Github Pages LINK === https://usheninte.github.io/react-forms/form.html
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+  handleSubmit(event) {
+
+  }
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />  
+        </label>  
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById("app")
+);
